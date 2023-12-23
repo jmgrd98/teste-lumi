4f3d3ae311ea4631218d5227e9e07c80bbc1f768
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-23 17:01:21 -0300

    Update script.py

--- script.py
+++ script.py
@@ -12,17 +12,13 @@ def extract_table_data(pdf_path):
             if table:
                 all_text.extend(table)
         return all_text
-        
 
 def extract_data_from_pdf(pdf_path):
     global id
     table_data = extract_table_data(pdf_path)
-    # print(table_data)
 
     text = extract_text(pdf_path)
-
     lines = text.split('\n')
-    # print(text)
 
     numero_cliente = None
     mes_referencia = None
@@ -34,28 +30,13 @@ def extract_data_from_pdf(pdf_path):
     energia_compensada_valor = None
     contrib_ilum_publica = None
 
-    # for row in table_data:
-    #         if row:  # Check if row is not None
-    #             if row[0] and "Energia Elétrica" in row[0]:
-    #                 print('ROW', row)
-    #                 energia_eletrica_quantidade = row[2]
-    #                 energia_eletrica_valor = row[3]
-    #             # Additional logic for other rows
-    #             if row[0] and "Contrib Ilum Publica Municipal" in row[0]:
-    #                 try:
-    #                     contrib_ilum_publica = float(row[3].replace(',', '.'))
-    #                 except ValueError as e:
-    #                     print(f"Error extracting Contrib Ilum Publica data: {e}")
-
-
-
     for i in range(len(lines)):
         if "Nº DO CLIENTE" in lines[i]:
             if i + 1 < len(lines):
                 numero_cliente_line = lines[i + 1].strip()
                 numero_cliente = numero_cliente_line.split()[0]
 
-        elif "Referente a" in lines[i]:
+        if "Referente a" in lines[i]:
             mes_referencia = lines[i + 1].strip()
             match = re.search(r'\b([A-Za-z]{3}/\d{4})\b', mes_referencia)
             if match:
@@ -63,69 +44,72 @@ def extract_data_from_pdf(pdf_path):
             else:
                 mes_referencia = None
 
-        elif "Energia Elétrica" in lines[i]:
+        if "Energia Elétrica" in lines[i] and not energia_eletrica_quantidade:
             try:
-                if i + 11 < len(lines) and i + 15 < len(lines):
-                    quantidade_str = lines[i + 10].replace(',', '.').replace(' ', '')
-                    valor_str = lines[i + 14].replace(',', '.').replace(' ', '')
-                    if id == 8:
-                        quantidade_str = lines[i + 11].replace(',', '.').replace(' ', '')
-                        valor_str = lines[i + 15].replace(',', '.').replace(' ', '')
-                    if id == 15:
-                        quantidade_str = lines[i + 12].replace(',', '.').replace(' ', '')
-                        valor_str = lines[i + 16].replace(',', '.').replace(' ', '')
-                    if id == 16:
-                        quantidade_str = lines[i + 13].replace(',', '.').replace(' ', '')
-                        valor_str = lines[i + 17].replace(',', '.').replace(' ', '')
-                    if quantidade_str and valor_str:
-                        energia_eletrica_quantidade = float(quantidade_str)
-                        energia_eletrica_valor = float(valor_str)
-                        break
+                quantidade_str = lines[i + 10].replace(',', '.').replace(' ', '')
+                valor_str = lines[i + 18].replace(',', '.').replace(' ', '')
+                if id == 8:
+                    quantidade_str = lines[i + 11].replace(',', '.').replace(' ', '')
+                    valor_str = lines[i + 19].replace(',', '.').replace(' ', '')
+                if id == 15:
+                    quantidade_str = lines[i + 12].replace(',', '.').replace(' ', '')
+                    valor_str = lines[i + 20].replace(',', '.').replace(' ', '')
+                if id == 16:
+                    quantidade_str = lines[i + 13].replace(',', '.').replace(' ', '')
+                    valor_str = lines[i + 21].replace(',', '.').replace(' ', '')
+                if quantidade_str and valor_str:
+                    energia_eletrica_quantidade = float(quantidade_str)
+                    energia_eletrica_valor = float(valor_str)
             except ValueError as e:
                 print(f"Conversion error: {e}")
 
-        elif "Energia SCEE s/ ICMS" in lines[i]:
+        if "Energia SCEE s/ ICMS" in lines[i] and not energia_scee_quantidade:
             try:
-                if i + 11 < len(lines) and i + 15 < len(lines):
-                    quantidade_str = lines[i + 10].replace(',', '.').replace(' ', '')
-                    print(quantidade_str)
-                    valor_str = lines[i + 14].replace(',', '.').replace(' ', '')
-                    print(valor_str)
-                    if id == 8:
-                        quantidade_str = lines[i + 11].replace(',', '.').replace(' ', '')
-                        valor_str = lines[i + 15].replace(',', '.').replace(' ', '')
-                    if quantidade_str and valor_str:
-                        energia_scee_quantidade = float(quantidade_str)
-                        energia_scee_valor = float(valor_str)
+                scee_quantidade_str = lines[i + 10].replace(',', '.').replace(' ', '')
+                scee_valor_str = lines[i + 18].replace(',', '.').replace(' ', '')
+                if id == 8:
+                    scee_quantidade_str = lines[i + 11].replace(',', '.').replace(' ', '')
+                    scee_valor_str = lines[i + 19].replace(',', '.').replace(' ', '')
+                if id == 15:
+                    scee_quantidade_str = lines[i + 12].replace(',', '.').replace(' ', '')
+                    scee_valor_str = lines[i + 20].replace(',', '.').replace(' ', '')
+                if id == 16:
+                    scee_quantidade_str = lines[i + 13].replace(',', '.').replace(' ', '')
+                    scee_valor_str = lines[i + 21].replace(',', '.').replace(' ', '')
+                if scee_quantidade_str and scee_valor_str:
+                    energia_scee_quantidade = float(scee_quantidade_str)
+                    energia_scee_valor = float(scee_valor_str)
             except ValueError as e:
                 print(f"Error extracting Energia SCEE data: {e}")
 
-        elif "Energia compensada GD I" in lines[i]:
+        if "Energia compensada GD I" in lines[i] and not energia_compensada_quantidade:
             try:
-                if i + 11 < len(lines) and i + 15 < len(lines):
-                    quantidade_str = lines[i + 10].replace(',', '.').replace(' ', '')
-                    valor_str = lines[i + 14].replace(',', '.').replace(' ', '')
-                    if id == 8:
-                        quantidade_str = lines[i + 11].replace(',', '.').replace(' ', '')
-                        valor_str = lines[i + 15].replace(',', '.').replace(' ', '')
-                    if quantidade_str and valor_str:
-                        energia_compensada_quantidade = float(quantidade_str)
-                        energia_compensada_valor = float(valor_str)
+                compensada_quantidade_str = lines[i + 10].replace(',', '.').replace(' ', '')
+                compensada_valor_str = lines[i + 14].replace(',', '.').replace(' ', '')
+                if id == 8:
+                    compensada_quantidade_str = lines[i + 11].replace(',', '.').replace(' ', '')
+                    compensada_valor_str = lines[i + 19].replace(',', '.').replace(' ', '')
+                if id == 15:
+                    compensada_quantidade_str = lines[i + 12].replace(',', '.').replace(' ', '')
+                    compensada_valor_str = lines[i + 20].replace(',', '.').replace(' ', '')
+                if id == 16:
+                    compensada_quantidade_str = lines[i + 13].replace(',', '.').replace(' ', '')
+                    compensada_valor_str = lines[i + 21].replace(',', '.').replace(' ', '')
+                if compensada_quantidade_str and compensada_valor_str:
+                    energia_compensada_quantidade = float(compensada_quantidade_str)
+                    energia_compensada_valor = float(compensada_valor_str)
             except ValueError as e:
                 print(f"Error extracting Energia compensada data: {e}")
 
-        elif "Contrib Ilum Publica Municipal" in lines[i]:
+        if "Contrib Ilum Publica Municipal" in lines[i] and not contrib_ilum_publica:
             try:
-                if i + 15 < len(lines):
-                    contrib_ilum_publica = float(lines[i + 15].replace(',', '.').replace(' ', ''))
-                    if id == 8:
-                        contrib_ilum_publica = float(lines[i + 16].replace(',', '.').replace(' ', ''))
+                contrib_str = lines[i + 15].replace(',', '.').replace(' ', '')
+                if contrib_str:
+                    contrib_ilum_publica = float(contrib_str)
             except ValueError as e:
                 print(f"Error extracting Contrib Ilum Publica data: {e}")
 
-
     return (numero_cliente, mes_referencia, energia_eletrica_quantidade, energia_eletrica_valor, energia_scee_quantidade, energia_scee_valor, energia_compensada_quantidade, energia_compensada_valor, contrib_ilum_publica)
-
 
 def insert_into_postgres(data):
     connection = psycopg2.connect(
@@ -140,9 +124,7 @@ def insert_into_postgres(data):
 
     try:
         insert_query = "INSERT INTO faturas (id, numero_cliente, mes_referencia, energia_eletrica_quantidade, energia_eletrica_valor, energia_scee_quantidade, energia_scee_valor, energia_compensada_quantidade, energia_compensada_valor, contrib_ilum_publica) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-
         cursor.execute(insert_query, data)
-
         connection.commit()
     except Exception as e:
         connection.rollback()
@@ -158,9 +140,7 @@ def process_pdfs(pdf_folder):
         id += 1
         if filename.endswith(".pdf"):
             pdf_path = os.path.join(pdf_folder, filename)
-            
             data = extract_data_from_pdf(pdf_path)
-
             if data:
                 data_with_id = (id,) + data
                 insert_into_postgres(data_with_id)
