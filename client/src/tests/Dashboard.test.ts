c499625c39ce113735cbc9ace322fb8336173aa9
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-24 14:55:10 -0300

    dashboard tests

--- client/src/tests/Dashboard.test.ts
+++ client/src/tests/Dashboard.test.ts
@@ -1,10 +1,28 @@
 import { describe, it, expect, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Dashboard from '../pages/Dashboard';
 import axios from 'axios';
 
-vi.mock('axios');
+const mockFaturas = [
+  {
+    "id": 1,
+		"numero_cliente": "7005400387",
+		"mes_referencia": "JUN/2023",
+		"energia_eletrica_quantidade": 50,
+		"energia_eletrica_valor": 43.28,
+		"energia_scee_quantidade": 1.007,
+		"energia_scee_valor": 652.55,
+		"energia_compensada_quantidade": 1.007,
+		"energia_compensada_valor": -620,
+		"contrib_ilum_publica": 43.28
+  }
+];
+
+vi.mock('axios', () => ({
+  get: vi.fn(() => Promise.resolve({ data: mockFaturas }))
+}));
 
 describe('Dashboard', () => {
   it('renders the dashboard title', () => {
@@ -12,6 +30,7 @@ describe('Dashboard', () => {
     const titleElement = screen.getByText('Dashboard');
     expect(titleElement).toBeInTheDocument();
   });
+});
 
   it('renders the customer number dropdown', () => {
     render(<Dashboard />);
@@ -26,23 +45,30 @@ describe('Dashboard', () => {
   });
 
   it('updates charts on selecting a customer number', async () => {
-    axios.get.mockResolvedValue({ data: [/* mocked data here */] });
     render(<Dashboard />);
-    // Here you would simulate selecting a customer number and check the charts update
+    const dropdown = screen.getByRole('combobox');
+    userEvent.selectOptions(dropdown, "7005400387"); // Mock selection
+    await waitFor(() => {
+      // Check that the charts update with mock data
+    });
   });
-
+  
   it('clears charts on selecting no customer number', async () => {
-    axios.get.mockResolvedValue({ data: [/* mocked data here */] });
     render(<Dashboard />);
-    // Here you would simulate selecting the "no customer number" option and check the charts clear
+    const dropdown = screen.getByRole('combobox');
+    userEvent.selectOptions(dropdown, ""); // Select no customer number
+    await waitFor(() => {
+      // Check that the charts clear out
+    });
   });
-
+  
   it('handles API errors gracefully', async () => {
     axios.get.mockRejectedValue(new Error('API error'));
     render(<Dashboard />);
     await waitFor(() => {
       // Check how your component handles and displays the error
+      const errorElement = screen.getByText(/API error/i);
+      expect(errorElement).toBeInTheDocument();
     });
-  });
 });
 
